669cf3f81b1dbb7bef9e933f816d6b3951fc79ca
gack951 <gack951@gmail.com>
2024-08-18 22:51:46 +0900

    first release

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -7,7 +7,15 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            #[cfg(desktop)]
+            app.handle()
+                .plugin(tauri_plugin_updater::Builder::new().build())?;
+            Ok(())
+        })
         .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_fs::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,72 @@
-import { invoke } from "@tauri-apps/api/core";
+import { open } from '@tauri-apps/plugin-dialog';
+import { readDir, copyFile } from '@tauri-apps/plugin-fs';
+import { basename, join } from '@tauri-apps/api/path';
+import { convertFileSrc } from '@tauri-apps/api/core';
+import { getCurrentWindow } from '@tauri-apps/api/window';
+import { getVersion } from "@tauri-apps/api/app";
 
-let greetInputEl: HTMLInputElement | null;
-let greetMsgEl: HTMLElement | null;
+getVersion().then(ver => {
+  getCurrentWindow().setTitle(`写真コピー2 [v${ver}]`);
+});
 
-async function greet() {
-  if (greetMsgEl && greetInputEl) {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    greetMsgEl.textContent = await invoke("greet", {
-      name: greetInputEl.value,
-    });
-  }
-}
+const source_folder_path_list = document.querySelector("#source_folder_path_list") as HTMLTextAreaElement;
+const destination_folder_path = document.querySelector("#destination_folder_path") as HTMLInputElement;
+const filename_entry_list = document.querySelector("#filename_entry_list") as HTMLTextAreaElement;
+const ambiguous_path_list = document.querySelector("#ambiguous_path_list") as HTMLSelectElement;
+const preview_img_wrapper = document.querySelector("#preview_img_wrapper") as HTMLDivElement;
 
 window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form")?.addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
+  document.querySelector("#add_folder_button")?.addEventListener("click", async _e => {
+    const selected = await open({
+      multiple: true,
+      directory: true,
+    });
+    if (selected === null) {
+      return;
+    }
+    source_folder_path_list.value += selected.join("\n") + "\n";
+  });
+  document.querySelector("#select_folder_button")?.addEventListener("click", async _e => {
+    const selected = await open({
+      directory: true,
+    });
+    if (selected === null) {
+      return;
+    }
+    destination_folder_path.value = selected;
+  });
+  let pending_ambiguous = (_v: string) => { };
+  document.querySelector("#copy_start_button")?.addEventListener("click", async _e => {
+    preview_img_wrapper.style.backgroundImage = "";
+    preview_img_wrapper.classList.remove("active");
+    ambiguous_path_list.textContent = "";
+    const all_files = (await Promise.all(source_folder_path_list.value.split(/\r?\n/).filter(dir => dir !== "").map(dir => readDir(dir).then(files => Promise.all(files.filter(file => file.isFile).map(file => join(dir, file.name))))))).flat();
+    const not_found_entries = [];
+    for await (const entry of filename_entry_list.value.split(/\r?\n/).filter(dir => dir !== "")) {
+      const hit_files = all_files.filter(path => path.includes(entry));
+      if (hit_files.length == 0) {
+        not_found_entries.push(entry);
+        continue;
+      }
+      if (hit_files.length > 1) {
+        ambiguous_path_list.textContent = "";
+        ambiguous_path_list.insertAdjacentHTML("beforeend", hit_files.map(path =>/*html*/`<option class="ambiuous_file_option" value="${path}">${path}</option>`).join());
+        (ambiguous_path_list.querySelector(".ambiuous_file_option") as HTMLOptionElement).selected = true;
+        ambiguous_path_list.dispatchEvent(new Event("input"));
+        hit_files[0] = await new Promise((res, _rej) => { pending_ambiguous = res; });
+      }
+      await copyFile(hit_files[0], await join(destination_folder_path.value, await basename(hit_files[0])));
+    }
+    filename_entry_list.value = not_found_entries.join("\n");
+    preview_img_wrapper.style.backgroundImage = "";
+    preview_img_wrapper.classList.remove("active");
+    ambiguous_path_list.textContent = "";
+  });
+  ambiguous_path_list.addEventListener("input", async _e => {
+    preview_img_wrapper.style.backgroundImage = `url(${convertFileSrc(ambiguous_path_list.value)})`;
+    preview_img_wrapper.classList.add("active");
+  });
+  preview_img_wrapper.addEventListener("click", async _e => {
+    pending_ambiguous(ambiguous_path_list.value);
   });
 });
